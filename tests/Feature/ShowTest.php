ef724306a15e346d07e14409dcf7ef1f9e35a8cd
alirezadp10 <alirezadp10@gmail.com>
2022-08-01 20:34:05 +0430

    fill show tests

--- tests/Feature/ShowTest.php
+++ tests/Feature/ShowTest.php
@@ -8,13 +8,26 @@ use Tests\TestCase;
 class ShowTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     /**
      * @test
      */
     public function show_can_be_created_by_user()
     {
-        $this->assertTrue(true);
+        $this->postJson('/show', [
+            'name'     => 'first show',
+            'times'    => [
+                [
+                    'day'  => 'sat',
+                    'time' => '15:00'
+                ],
+                [
+                    'day'  => 'fri',
+                    'time' => '13:30'
+                ],
+            ],
+            'capacity' => 10
+        ])->assertCreated();
     }
 
     /**
@@ -22,22 +35,81 @@ class ShowTest extends TestCase
      */
     public function multiple_shows_cannot_hold_at_the_same_time()
     {
-        $this->assertTrue(true);
+        \App\Models\TimeTable::factory()->create([
+            'day'  => 'sat',
+            'time' => '13:00',
+        ]);
+
+        $response = $this->postJson('/show', [
+            'name'     => 'second show',
+            'times'    => [
+                [
+                    'day'  => 'sat',
+                    'time' => '13:00'
+                ],
+                [
+                    'day'  => 'fri',
+                    'time' => '13:30'
+                ],
+            ],
+            'capacity' => 10
+        ]);
+
+        $response->assertJsonValidationErrors([
+            'times.0' => 'multiple shows cannot hold at the same time'
+        ]);
     }
-    
+
     /**
      * @test
      */
     public function shows_capacity_cannot_be_less_than_5_people()
     {
-        $this->assertTrue(true);
+        $response = $this->postJson('/show', [
+            'name'     => 'second show',
+            'times'    => [
+                [
+                    'day'  => 'sat',
+                    'time' => '13:00'
+                ],
+                [
+                    'day'  => 'fri',
+                    'time' => '13:30'
+                ],
+            ],
+            'capacity' => 4
+        ]);
+
+        $response->assertJsonValidationErrors([
+            'capacity' => 'shows capacity cannot be less than 5 people'
+        ]);
     }
-    
+
     /**
      * @test
      */
     public function shows_list_must_be_reachable_in_pagination_with_the_size_of_5()
     {
-        $this->assertTrue(true);
+        \App\Models\Show::factory(17)->create();
+
+        $response = $this->getJson('/show')->assertOk()->assertJsonStructure([
+            'data' => [
+                [
+                    "id",
+                    "name",
+                    "times" => [
+                        [
+                            'day',
+                            'time'
+                        ]
+                    ],
+                    "capacity",
+                    "updated_at",
+                    "created_at",
+                ],
+            ],
+        ])->assertJsonCount(5, 'data');
+
+        $this->assertCount(17, $response->json('data.total'));
     }
 }
